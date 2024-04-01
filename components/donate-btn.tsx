c6530e9e694d637bb2fb372f5d7dcfc966332b84
Kaiyrkhan Kairolla <111349192+sogeking7@users.noreply.github.com>
2024-04-01 17:08:42 +0500

    Change KASPI_LINK

--- components/donate-btn.tsx
+++ components/donate-btn.tsx
@@ -2,12 +2,11 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
+const KASPI_LINK = "https://pay.kaspi.kz/pay/acvl2j7n";
+
 export const DonateButton = ({ text }: { text: string }) => {
   return (
-    <Link
-      className="mt-12 w-full rounded-md"
-      href="https://kaspi.kz/pay/ASARUME"
-    >
+    <Link className="mt-12 w-full rounded-md" href={KASPI_LINK}>
       <Button className="w-full" variant="kaspi">
         <Image
           alt="kaspi"
